f8d2411731300f1a367e9273477386303d231d4b
Mohammed Yusuf <mohammedyusuf445@gmail.com>
2023-09-17 22:36:05 +0300

    Done

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,47 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
-
+    i = 10
+    while i >= 1:
+        print(i)
+        i -= 1
+        print("Happy New Year!")
 def square_integers(int_list):
     # code goes here!
-    pass
+    squared_numbers = [x**2 for x in int_list]
+    return squared_numbers
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for i in range(1,101):
+        if i % 5 == 0 and i % 3 == 0:
+           print ("FizzBuzz")
+        elif i % 3 == 0:
+            print ("Fizz")
+        elif i % 5 == 0:
+            print ("Buzz")
+        else :
+            print(i)
+            
+            
+
+# i = 0
+# while i<5:
+#     print("looping")
+#     i += 1
+
+# for i in range(10):
+#     print("looping")
+#     print(f"i is: {i}")
+
+            
+        
+            
+        
+        
+            
+                
+            
+            
+            
+        
